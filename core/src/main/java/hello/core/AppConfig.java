127c413d11a6b0d480ac010f2fed37250aedc023
ylpitseleh <lyla02217@gmail.com>
2020-10-14 16:16:02 +0900

    Add all

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -46,4 +46,6 @@ public class AppConfig {
         return new FixDiscountPolicy();
     }
 
+
+
 }
--- core/src/test/java/hello/core/XmlAppContext.java
+++ core/src/test/java/hello/core/XmlAppContext.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class XmlAppContext {
 
     @Test
-    void xmpAppContext() {
+    public void xmpAppContext() {
         ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
         MemberService memberService = ac.getBean("memberService", MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
